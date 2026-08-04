310204d1c8bc0227d7beccfdf965c34a3ef0ca76
shekaderitesh <shekaderitesh@gmail.com>
2026-08-04 22:51:02 +0530

    Add files via upload

---
+++ C_Programming/Address.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+    int no=21;
+
+    printf("%d\n",no);               // 21
+    printf("%d\n",sizeof(no));       // 4
+    printf("%d\n",&no);              // 1000
+
+
+
+    return 0;
+    
+}
