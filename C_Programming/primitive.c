85a6ea5f21cf6d5e2f49f68045b623686d9be18d
shekaderitesh <shekaderitesh@gmail.com>
2026-08-04 22:46:58 +0530

    Add files via upload

---
+++ C_Programming/primitive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+      char ch= 'A';
+      int i= 11;
+      float no = 3.14f;
+      double d= 90.78567;
+
+      printf("%c\n",ch);
+      printf("%d\n",i);
+      printf("%f\n",no);
+      printf("%lf\n",d);
+
+
+    return 0;
+    
+}
