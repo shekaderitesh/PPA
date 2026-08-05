d5124e6557fa1b253e79638d5d5282296edaf135
shekaderitesh <shekaderitesh@gmail.com>
2026-08-05 22:24:42 +0530

    Add files via upload

---
+++ C_Programming/Addition.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+{
+
+    int No1=0, No2=0, Ans=0;
+    printf("Enter first no:\n");
+    scanf("%d",&No1);
+
+    printf("Enter Second  no:\n");
+    scanf("%d",&No2);
+
+    Ans =No1 + No2;                 
+    printf("Addition is %d\n",Ans);
+
+    return 0;
+
+}
