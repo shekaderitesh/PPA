35f1fd032e02e91979e61697ab99c9e34f79ee3b
shekaderitesh <shekaderitesh@gmail.com>
2026-08-25 22:59:35 +0530

    Add files via upload

---
+++ C++_Programming/StaticDemo1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int No1;
+        int No2;
+        static int x;
+
+};
+
+int main()
+{
+    Demo obj1;
+    Demo obj2;
+
+    cout<<sizeof(obj1);
+    
+
+}
