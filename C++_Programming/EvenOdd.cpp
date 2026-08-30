4c97060ccf7cecf988847cc2919386c21688c1cc
shekaderitesh <shekaderitesh@gmail.com>
2026-08-30 10:26:23 +0530

    Add files via upload

---
+++ C++_Programming/EvenOdd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int no;
+
+    cout<<"Enter Number:";
+    cin>>no;
+
+    if (no% 2 == 0)
+    {
+        cout<<"Number is Even: ";
+        }
+    else
+    {
+        cout<<"Numer is odd: ";
+
+
+    }
+    return 0;
+    
+}
