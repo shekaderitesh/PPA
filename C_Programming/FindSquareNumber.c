c9c5d30b18476c20647e2e8c8a290656a7b65300
shekaderitesh <shekaderitesh@gmail.com>
2026-08-30 23:30:06 +0530

    Add files via upload

---
+++ C_Programming/FindSquareNumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int no, Square;
+
+    printf("Enter Number: ");
+    scanf("%d\n", &no);
+
+    Square = no * no;
+
+    printf("Square %d", Square);
+
+    return 0;
+}
