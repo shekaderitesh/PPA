a69ca95bf236ef4dafb3e0f7e579fb1ec07d2335
shekaderitesh <shekaderitesh@gmail.com>
2026-09-02 09:49:48 +0530

    Add files via upload

---
+++ C++_Programming/SingleLevelX.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+       int i, j;
+
+       Base()
+       {
+          cout<<"Inside Base constructor\n";
+       }
+
+       ~Base()
+       {
+          cout<<"Inside Base destructor\n";
+       }
+
+       void fun()
+       {
+        cout<<"Inside Base fun\n";
+       }
+
+       void gun()
+       {
+        cout<<"Inside Base gun\n";
+       }
+};
+
+class Derived : public Base
+{
+     public:
+         int x, y;
+
+         Derived()
+         {
+            cout<<"Inside Derived constructor\n";
+         }
+
+         ~Derived()
+         {
+            cout<<"Inside Derived destructor\n";
+         }
+
+         void sun()
+         {
+            cout<<"Inside Derived desyructor ";
+         }
+};
+
+int main()
+{
+   Derived dobj;
+
+   dobj.fun();
+   dobj.gun();
+   dobj.sun();
+;   
+        
+   
+   return 0;
+}
