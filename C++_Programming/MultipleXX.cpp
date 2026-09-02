7ce33ae14c05c1593db7190070d7ef062df14d33
shekaderitesh <shekaderitesh@gmail.com>
2026-09-03 00:06:35 +0530

    Add files via upload

---
+++ C++_Programming/MultipleXX.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+class BaseA
+{
+    public : 
+       int i , j;
+
+       BaseA()
+       {
+        cout<<"Inside BaseA constructor\n";
+       }
+
+       ~BaseA()
+       {
+        cout<<"Inside BaseA destructor\n";
+       }
+
+       void fun()
+       {
+        cout<<"Inside BaseA fun\n";
+       }
+};
+
+class BaseB
+{
+    public : 
+       int x , y;
+
+       BaseB()
+       {
+        cout<<"Inside BaseB constructor\n";
+       }
+
+       ~BaseB()
+       {
+        cout<<"Inside BaseB destructor\n";
+       }
+
+       void gun()
+       {
+        cout<<"Inside BaseB gun\n";
+       }
+};
+
+class Derived : public BaseB, public BaseA
+{
+    public:
+        int a;
+
+        Derived()
+        {
+            cout<<"Inside Derived constructor\n";
+        }
+
+        ~Derived()
+        {
+            cout<<"Inside Derived destructor\n";
+        }
+
+        void sun()
+        {
+            cout<<"Inside Derived sun\n";
+        }
+
+};
+
+
+int main()
+{
+    Derived dobj;
+    
+    dobj.fun();
+    dobj.gun();
+    dobj.sun();
+
+    return 0;
+}
