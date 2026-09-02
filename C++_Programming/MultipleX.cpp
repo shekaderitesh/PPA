973f1a1ae7c456c1bb545187d336428c57d9e856
shekaderitesh <shekaderitesh@gmail.com>
2026-09-03 00:05:40 +0530

    Add files via upload

---
+++ C++_Programming/MultipleX.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class BaseA
+{
+    public : 
+       int i , j;
+
+       BaseA()
+       {
+        cout<<"Inside BaseA constructor\n";
+       }
+
+       ~BaseA()
+       {
+        cout<<"Inside BaseA destructor\n";
+       }
+
+       void fun()
+       {
+        cout<<"Inside BaseA fun\n";
+       }
+};
+
+class BaseB
+{
+    public : 
+       int x , y;
+
+       BaseB()
+       {
+        cout<<"Inside BaseB constructor\n";
+       }
+
+       ~BaseB()
+       {
+        cout<<"Inside BaseB destructor\n";
+       }
+
+       void gun()
+       {
+        cout<<"Inside BaseB gun\n";
+       }
+};
+
+class Derived : public BaseA, BaseB
+{
+    public:
+        int a;
+
+        Derived()
+        {
+            cout<<"Inside Derived constructor\n";
+        }
+
+        ~Derived()
+        {
+            cout<<"Inside Derived destructor\n";
+        }
+
+        void sun()
+        {
+            cout<<"Inside Derived sun\n";
+        }
+
+};
+
+
+int main()
+{
+    Derived dobj;
+    
+    return 0;
+}
