25092fe157761797cd13cb7886533ed29c5bf488
shekaderitesh <shekaderitesh@gmail.com>
2026-09-04 11:31:31 +0530

    Add files via upload

---
+++ Java_Programming/TableOf2.java
@@ -0,0 +1,10 @@
+class Main 
+{
+    public static void main(String A[]) 
+    {
+        for (int i = 1; i <= 10; i++) 
+        {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
