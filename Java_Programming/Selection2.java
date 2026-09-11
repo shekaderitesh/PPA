90d9c1a6d34f384b98e1090c1746d70baf3e5df3
shekaderitesh <shekaderitesh@gmail.com>
2026-09-11 19:35:22 +0530

    Add files via upload

--- Java_Programming/Selection2.java
+++ Java_Programming/Selection2.java
@@ -1,21 +1,30 @@
-import java.util.*;//*means import all
+import java.util.*;
 class Selection2
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        int no1 , no2 , ans;
+
         Scanner sobj = new Scanner(System.in);
-        int no = 0;
 
-        System.out.println("Enter You Age:");
-        no=sobj.nextInt();
+        System.out.println("Enter first number");
+        no1 = sobj.nextInt();
 
-        if (no>=18)
+        System.out.println("Enter Second number");
+        no2 = sobj.nextInt();
+
+        ans = no1 + no2;
+
+        if (ans>100)
         {
-            System.out.println("You are allowed to watch movie");
+            System.out.println("Sum of these two number is Greater then 100");
         }
         else
         {
-            System.out.println("You are not allowed to watch movie");
+            System.out.println("Sum of these two number is less then 100");
         }
+
+
+
+
     }
 }
