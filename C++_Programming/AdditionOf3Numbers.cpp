abbbdea3f6dab24cba9eca09c6505517fdb204c8
shekaderitesh <shekaderitesh@gmail.com>
2026-09-17 19:23:36 +0530

    Add files via upload

---
+++ C++_Programming/AdditionOf3Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+
+    cout << "Addition = " << a + b + c;
+
+    return 0;
+}
