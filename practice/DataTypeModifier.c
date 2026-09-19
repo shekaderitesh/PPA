b0eab71a9d05f406b4cf99a1ad5311926a258f73
shekaderitesh <shekaderitesh@gmail.com>
2026-09-19 18:51:25 +0530

    Add files via upload

---
+++ practice/DataTypeModifier.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    short int b = 20;
+    long int c = 30;
+    unsigned int d = 40;
+
+    printf("%d\n", a);
+    printf("%hd\n", b);
+    printf("%ld\n", c);
+    printf("%u\n", d);
+
+    return 0;
+}
