1a0f7dd1f8b8852f3d036180b3d1c580936430fb
shekaderitesh <shekaderitesh@gmail.com>
2026-09-20 16:15:46 +0530

    Add files via upload

---
+++ practice/DataTypeModifier.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    signed int a = -10;
+    unsigned int b = 20;
+    short int c = 100;
+    long int d = 100000;
+    long long int e = 1000000000;
+
+    cout << a << " " << b << " " << c << " " << d << " " << e;
+    return 0;
+}
