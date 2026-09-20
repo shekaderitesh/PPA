b0d2f50f0646d4152334086c565a0094e4e334ac
shekaderitesh <shekaderitesh@gmail.com>
2026-09-20 16:16:11 +0530

    Add files via upload

---
+++ practice/DataTypeModifier.java
@@ -0,0 +1,9 @@
+public class Main {
+    static int a = 10;
+    final int b = 20;
+    private long c = 100000L;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
