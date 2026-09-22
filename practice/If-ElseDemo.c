71656db71cb0f72fe72e9ec0c2f6f81211a34175
shekaderitesh <shekaderitesh@gmail.com>
2026-09-22 10:43:31 +0530

    Add files via upload

---
+++ practice/If-ElseDemo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;
+
+    if (number > 0) {
+        printf("Positive number");
+    } else {
+        printf("Negative number");
+    }
+
+    return 0;
+}
