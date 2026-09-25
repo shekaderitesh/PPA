2dec27d975dac113b758412c403ee2e5d4a7abc2
shekaderitesh <shekaderitesh@gmail.com>
2026-09-25 16:16:21 +0530

    Add files via upload

---
+++ practice/ArrayUsePointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+
+    printf("Array elements using pointer:\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
